68e2dba37cfad0159ccae935790e14b7e7740b5d
Vaish <vaishakhd0306@gmail.com>
2025-04-16 20:18:51 +0530

    firstProject

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const signUpButton = document.querySelector(".green-bg button");
+
+signUpButton.addEventListener("click",()=>{
+    container.classList.toggle("change");
+    console.log("clicked")
+})
